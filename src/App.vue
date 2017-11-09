b7bbd1f66761b7cf5c28dc238fa91bf9406916c0
RCbizbox <rc@bizbox.ph>
2017-11-09 16:11:12 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+  <v-navigation-drawer v-model="sideNav">
+    <v-list dense>
+      <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>View Meet-ups</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+    </v-list>
+  </v-navigation-drawer> 
+    <v-toolbar class="blue lighten-4"> 
+      <v-toolbar-side-icon @click.native.stop="sideNav=!sideNav"
+      class="hidden-sm-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>
+        Meet-APP
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+       <v-btn flat>
+       <v-icon left> supervisor_account</v-icon>
+        View Meet-ups
+       </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
