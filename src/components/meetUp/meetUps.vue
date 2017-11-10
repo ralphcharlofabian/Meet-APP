6da331860d25ac7d050bf5679f7cbaec9c4ac719
RCbizbox <rc@bizbox.ph>
2017-11-10 17:12:03 +0800

    update

--- src/components/meetUp/meetUps.vue
+++ src/components/meetUp/meetUps.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        <p>The Meetups Page</p>
-    </div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card class="info">
+                    <v-container fluid>
+                        <v-layout row>
+                            <v-flex xs5>
+                                <v-card-media
+                                    src="/static/doc-images/cards/foster.jpg"
+                                    height="125px"
+                                    contain
+                                    ></v-card-media>
+                                    3:11
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
